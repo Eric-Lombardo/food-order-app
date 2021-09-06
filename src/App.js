e2406ffbe10e70a9afa0acc0d86d09c95d1583ba
Eric Lombardo <elombardo@bkom.com>
2021-09-06 10:51:59 -0400

    Create Context to store app-wide state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [isVisible, setIsVisible] = useState(false);
@@ -11,13 +12,13 @@ function App() {
   };
 
   return (
-    <>
+    <CartProvider>
       {isVisible && <Cart onCartDisplay={cartHandler} />}
       <Header onCartDisplay={cartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
